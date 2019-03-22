16efeb5b08e089f200261f8b84859275152aea38
Jesse <zhaoyifei505@gmail.com>
2019-03-22 21:54:18 +0800

    init Arouter in BaseApp

---
+++ share/src/main/java/com/loong/share/ShareApp.java
@@ -0,0 +1,17 @@
+package com.loong.share;
+
+import android.app.Application;
+
+import com.loong.base.BaseApp;
+
+public class ShareApp extends BaseApp {
+    @Override
+    public void initModuleApp(Application application) {
+
+    }
+
+    @Override
+    public void initModuleData(Application application) {
+
+    }
+}
