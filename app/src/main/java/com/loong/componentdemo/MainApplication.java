0bae4202a649df3a409135eac79eedb0cf9177da
Jesse <zhaoyifei505@gmail.com>
2019-03-22 21:41:59 +0800

    init Arouter in BaseApp

--- app/src/main/java/com/loong/componentdemo/MainApplication.java
+++ app/src/main/java/com/loong/componentdemo/MainApplication.java
@@ -11,20 +11,10 @@ public class MainApplication extends BaseApp {
     public void onCreate() {
         super.onCreate();
 
-        // 初始化 ARouter
-        if (isDebug()) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
-            ARouter.openLog();     // 打印日志
-            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-        }
-        ARouter.init(this);
-
         initModuleApp(this);
         initModuleData(this);
     }
 
-    private boolean isDebug() {
-        return BuildConfig.DEBUG;
-    }
 
     @Override
     public void initModuleApp(Application application) {
--- base/src/main/java/com/loong/base/BaseApp.java
+++ base/src/main/java/com/loong/base/BaseApp.java
@@ -1,8 +1,26 @@
 package com.loong.base;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.alibaba.android.arouter.launcher.ARouter;
 
 public abstract class BaseApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 初始化 ARouter
+        if (isDebug()) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this);
+        Log.i("zhaoyifei", "初始化router");
+    }
+
+    private boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
     /**
      * Application 初始化
      */
